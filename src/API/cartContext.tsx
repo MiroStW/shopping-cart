b27997980a9db889137f3ea7017f726a54359be7
fea17e86 <fea17e86@mail.de>
2021-11-21 09:36:40 +0100

    improves cart state context

--- src/API/cartContext.tsx
+++ src/API/cartContext.tsx
@@ -14,9 +14,13 @@ const CartStateContext = React.createContext<
   { state: State; dispatch: Dispatch } | undefined
 >(undefined);
 
-function getLocalStorage() {
-  if (localStorage.getItem("shopping-cart"))
-    return JSON.parse(localStorage.getItem("shopping-cart") as string);
+// you missed the type declarations here, otherwise it is producing any
+function getLocalStorage(): State {
+  // caling it twice is a potential performance issue (if you do it a lot)
+  // the TypeScript complaint can easily prevented by extracting the retreived value
+  const persistedState = localStorage.getItem("shopping-cart");
+  if (persistedState)
+    return JSON.parse(persistedState);
   else return { cartItems: [] };
 }
 
@@ -24,7 +28,7 @@ function setLocalStorage(state: State) {
   if (state) localStorage.setItem("shopping-cart", JSON.stringify(state));
 }
 
-function cartReducer(state: State, action: Action) {
+function cartReducer(state: State, action: Action): State {
   switch (action.type) {
     case "ADD_TO_CART": {
       const targetProductPosition = state.cartItems.findIndex(
@@ -40,23 +44,41 @@ function cartReducer(state: State, action: Action) {
           ],
         };
         // setCartItems((prevState) => [...prevState, { product, quantity }]);
-      } else {
-        state.cartItems[targetProductPosition].quantity += action.quantity;
-        return { ...state, cartItems: [...state.cartItems] };
       }
+      // skipping the else statement when the if had a return makes the code more readable
+
+        // You shouldn't mutate the state here, rather create new state.
+        // I don't love this solution for potentially unnecessary loops,
+        // but it is very readable and does the job.
+        // So if you can't see any actual performance hits, I wouldn't worry too much about it.
+        // Something like https://immerjs.github.io/immer/
+        // or https://github.com/kolodny/immutability-helpermight
+        // might be helpful with more complex immutable data structures
+        // (haven't tried any of them though)
+        const updatedItems = state.cartItems.map((cartItem, index) => {
+          if (index === targetProductPosition) {
+            return { ...cartItem, quantity: cartItem.quantity + action.quantity };
+          }
+          return cartItem;
+        })
+        return { ...state, cartItems: updatedItems };
     }
     case "UPDATE_QUANTITY": {
-      const targetProductPosition = state.cartItems.findIndex(
-        (item) => item.product.id === action.product.id
-      );
+      // same here -> the state handled by a reducer should be immutable
+      if (action.quantity === 0) {
+        return { ...state, cartItems: state.cartItems.filter((cartItem) => cartItem.product.id === action.product.id) };
+      }
 
-      if (action.quantity === 0)
-        state.cartItems.splice(targetProductPosition, 1);
-      else state.cartItems[targetProductPosition].quantity = action.quantity;
-
-      return { ...state, cartItems: [...state.cartItems] };
+      const updatedItems = state.cartItems.map((cartItem) => {
+        if (cartItem.product.id === action.product.id) {
+          return { ...cartItem, quantity: action.quantity };
+        }
+        return cartItem;
+      })
+      return { ...state, cartItems: updatedItems };
     }
     case "DELETE_ITEM": {
+      // same here -> I didn't change the code this time
       const targetProductPosition = state.cartItems.findIndex(
         (item) => item.product.id === action.product.id
       );
@@ -75,6 +97,7 @@ function CartProvider({ children }: CartProviderProps) {
   // NOTE: you *might* need to memoize this value
   // Learn more in http://kcd.im/optimize-context
   const value = { state, dispatch };
+  // are you aware it sets the local state on every render?
   setLocalStorage(state);
   return (
     <CartStateContext.Provider value={value}>
