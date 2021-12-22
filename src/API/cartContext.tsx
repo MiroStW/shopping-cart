14a141f4025df9e753448bbd03cd5b567a29dd9b
Miro <miro@miro-wilms.de>
2021-12-22 14:22:14 +0100

    removed comments, fixed logic error in L69

--- src/API/cartContext.tsx
+++ src/API/cartContext.tsx
@@ -14,13 +14,9 @@ const CartStateContext = React.createContext<
   { state: State; dispatch: Dispatch } | undefined
 >(undefined);
 
-// you missed the type declarations here, otherwise it is producing any
 function getLocalStorage(): State {
-  // caling it twice is a potential performance issue (if you do it a lot)
-  // the TypeScript complaint can easily prevented by extracting the retreived value
   const persistedState = localStorage.getItem("shopping-cart");
-  if (persistedState)
-    return JSON.parse(persistedState);
+  if (persistedState) return JSON.parse(persistedState);
   else return { cartItems: [] };
 }
 
@@ -43,30 +39,23 @@ function cartReducer(state: State, action: Action): State {
             { product: action.product, quantity: action.quantity },
           ],
         };
-        // setCartItems((prevState) => [...prevState, { product, quantity }]);
       }
-      // skipping the else statement when the if had a return makes the code more readable
-
-        // You shouldn't mutate the state here, rather create new state.
-        // I don't love this solution for potentially unnecessary loops,
-        // but it is very readable and does the job.
-        // So if you can't see any actual performance hits, I wouldn't worry too much about it.
-        // Something like https://immerjs.github.io/immer/
-        // or https://github.com/kolodny/immutability-helpermight
-        // might be helpful with more complex immutable data structures
-        // (haven't tried any of them though)
-        const updatedItems = state.cartItems.map((cartItem, index) => {
-          if (index === targetProductPosition) {
-            return { ...cartItem, quantity: cartItem.quantity + action.quantity };
-          }
-          return cartItem;
-        })
-        return { ...state, cartItems: updatedItems };
+      const updatedItems = state.cartItems.map((cartItem, index) => {
+        if (index === targetProductPosition) {
+          return { ...cartItem, quantity: cartItem.quantity + action.quantity };
+        }
+        return cartItem;
+      });
+      return { ...state, cartItems: updatedItems };
     }
     case "UPDATE_QUANTITY": {
-      // same here -> the state handled by a reducer should be immutable
       if (action.quantity === 0) {
-        return { ...state, cartItems: state.cartItems.filter((cartItem) => cartItem.product.id === action.product.id) };
+        return {
+          ...state,
+          cartItems: state.cartItems.filter(
+            (cartItem) => cartItem.product.id !== action.product.id
+          ),
+        };
       }
 
       const updatedItems = state.cartItems.map((cartItem) => {
@@ -74,17 +63,16 @@ function cartReducer(state: State, action: Action): State {
           return { ...cartItem, quantity: action.quantity };
         }
         return cartItem;
-      })
+      });
       return { ...state, cartItems: updatedItems };
     }
     case "DELETE_ITEM": {
-      // same here -> I didn't change the code this time
-      const targetProductPosition = state.cartItems.findIndex(
-        (item) => item.product.id === action.product.id
-      );
-      state.cartItems.splice(targetProductPosition, 1);
-
-      return { ...state, cartItems: [...state.cartItems] };
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.product.id !== action.product.id
+        ),
+      };
     }
     // default: {
     //   throw new Error(`Unhandled action type: ${action.type}`);
